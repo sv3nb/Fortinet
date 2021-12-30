44b44a47e9294a580b7fc81a453151456c596dcc
sv3nb <91876383+sv3nb@users.noreply.github.com>
2021-12-30 19:25:18 +0100

    FortiOS API scripts ready!

--- API/create_address.py
+++ API/create_address.py
@@ -4,48 +4,43 @@ import csv
 from requests.auth import HTTPBasicAuth
 requests.packages.urllib3.disable_warnings()
 
-def Get_Address(url, key):
-    url = url
-    key = key
-    headers = {'content-type':'application/json', 'Accept': 'application/json'}
-    url = 'https://192.168.108.100/api/v2/cmdb/firewall/address/?access_token=8bNyng9Nm0t8550cr03ypf57HsjdnG&format=name|comment'
+def Get_Address():
+
+    http_header = {'content-type':'application/json', 'Accept': 'application/json'}
+    url = 'https://16.170.227.201/api/v2/cmdb/firewall/address/?access_token=wfnG5Nfmdy933gfnsN735q68nhGcqw&format=name'
     payload = {}
-    response = requests.get(url, data=json.dumps(payload), headers=headers, verify=False)
+    response = requests.get(url, data=json.dumps(payload), headers=http_header, verify=False)
     address_dict = json.loads(response.text)
     address_list = address_dict['results']
     return address_list
 
-def Create_Address(url, key):
-    url = url
-    key = key
+def Create_Address():
+
+    address_list = Get_Address()
     f = open('subnets.csv')
     reader = csv.DictReader(f, delimiter=';')
     headers = reader.fieldnames
     subnets = []
     for row in reader:
     	subnets.append(row)
-
-    address_list = Get_Address()
-    headers = {'content-type':'application/json', 'Accept': 'application/json'}
-    url = 'https://192.168.108.100/api/v2/cmdb/firewall/address/?access_token=8bNyng9Nm0t8550cr03ypf57HsjdnG'
+    http_header = {'content-type':'application/json', 'Accept': 'application/json'}
+    url = 'https://16.170.227.201/api/v2/cmdb/firewall/address/?access_token=wfnG5Nfmdy933gfnsN735q68nhGcqw'
 
     for subnet in subnets:
         name = str("n-" + subnet['IP range'] + subnet['Subnet mask'])
         cidr = str(subnet['IP range'] + subnet['Subnet mask'])
-        for address in address_list:
-            if name in address['name']:
-                pass
-            else:
-                payload =
-                {
-                    "name": name,
-                    "type": "ipmask",
-                    "subnet": cidr,
-                    "comment": subnet['Comment'],
-                    "color": 19
+        if name in json.dumps(address_list):
+                print(f"address already exists, skipping {name}")
+        else:
+            payload = {
+                "name": name,
+                "type": "ipmask",
+                "subnet": cidr,
+                "comment": subnet['Comment'],
+                "color": 19
                 }
-        response = requests.post(url, data=json.dumps(payload), headers=headers, verify=False)
-        print(response.text)
+            response = requests.post(url, data=json.dumps(payload), headers=http_header, verify=False)
+            print(response.text)
 
 if __name__ == "__main__":
     Create_Address()
